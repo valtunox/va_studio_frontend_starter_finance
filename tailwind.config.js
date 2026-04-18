e87b441f86e7fbb89306c9fd045b15e2125523a8
joelwembo <joelwembo@users.noreply.github.com>
2026-04-18 16:57:52 +0800

    sbd: sync 20260418T085753Z

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
-    './templates/**/*.{js,ts,jsx,tsx}',
+    './templates/finance/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
